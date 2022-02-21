85399cd22a4d21a95f5d0fa6f264353ffd23ea81
maciej rakowski <maciek_9822@wp.pl>
2022-02-21 17:24:54 +0100

    javascript-7

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let firstNumber = 24;
+
+let secondNumber = 66;
+
+function multiply(num1, num2) {
+    let suma = num1 + num2;
+
+    return suma;
+}
+
+console.log( multiply (firstNumber, secondNumber) );
